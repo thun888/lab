80aa722160dead4c8b0e9958e387bbcf93676e9e
thun888 <63234268+thun888@users.noreply.github.com>
2021-11-26 19:57:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,15 +6,12 @@ import (
 	"time"
 )
 
-//以每秒300次的速度向http://www.pkuzj.com/ 发送get请求
+
 func main() {
 
 	for {
-		//获取当前时间
 		now := time.Now()
-		url := "http://www.baidu.com/" + now.String()
-		//输出url到控制台
-		//一直循环
+		url := "http://www.pkuzj.com/" + now.String()
 		go func() {
 			resp, err := http.Get(url)
 			if err != nil {
